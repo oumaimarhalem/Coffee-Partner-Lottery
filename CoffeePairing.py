b1e7185fc72ef80a70f95f8042330c6878be7bd6
Marijn de Ruiter <marijnthomas@gmail.com>
2025-03-11 12:30:32 +0100

    Add API to forms

--- CoffeePairing.py
+++ CoffeePairing.py
@@ -5,14 +5,39 @@ import copy
 import os
 
 
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import client, file, tools
+import json
+
+"""
+All code for google forms is from google developers pages. I did not wrote it.
+"""
 
 
+SCOPES = "https://www.googleapis.com/auth/forms.responses.readonly"
+DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
+
+store = file.Storage("token.json")
+creds = None
+if not creds or creds.invalid:
+  flow = client.flow_from_clientsecrets("test.json", SCOPES)
+  creds = tools.run_flow(flow, store)
+
+service = discovery.build(
+    "forms",
+    "v1",
+    http=creds.authorize(Http()),
+    discoveryServiceUrl=DISCOVERY_DOC,
+    static_discovery=False,
+    )
 
 
+# Prints the responses of your specified form:
+form_id = "1eVElX0Ci_jFXhPhmety9IeySqea_B63JYceqm62MN70"
+result = service.forms().responses().list(formId=form_id).execute()
+print(result)
 
-#TEst
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
 
 
 
